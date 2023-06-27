1bbf875382e65f6b2a4be4d0fd249cbd7a6ab04b
mKUFUXn82tVispS <imran.alam@bwtech.co>
2023-06-27 12:37:57 -0700

    Images compress and League model class name case change

--- app/Models/league.php
+++ app/Models/league.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class League extends Model
+class league extends Model
 {
     use HasFactory;
 
