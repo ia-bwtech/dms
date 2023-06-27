9c5cddb1b32c04b177140f462a7b4b85eead6cc4
mKUFUXn82tVispS <imran.alam@bwtech.co>
2023-06-27 13:12:00 -0700

    League file name and class name issue resolved

--- app/Models/League.php
+++ app/Models/League.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class league extends Model
+class League extends Model
 {
     use HasFactory;
 
