b01ee016ec6b57c1c26262aae97099d08a9c56cc
mKUFUXn82tVispS <imran.alam@bwtech.co>
2023-07-05 13:54:00 -0700

    Telescope prune daily schedule active

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         // $schedule->command('telescope:prune --hours=48')->daily();
+        $schedule->command('telescope:prune')->daily();
         $schedule->command('deactivate:cron')->everyMinute();
         $schedule->command('odds:fetch')->everyMinute()->withoutOverlapping();
         $schedule->command('bets:grade')->everyTenMinutes()->withoutOverlapping();
